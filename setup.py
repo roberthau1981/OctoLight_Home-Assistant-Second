d8674191d1914c4c5e7591b26ad110d6dbea6ffb
roberthau1981 <robhau@gmail.com>
2024-01-18 17:42:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "octolightHA"
+plugin_identifier = "octolightHA2"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_octolightHA"
+plugin_package = "octoprint_octolightHA2"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoLight Home Assistant"
+plugin_name = "OctoLight Home Assistant2"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.0.4"
